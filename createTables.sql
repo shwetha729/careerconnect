18539c0ea7ad5035953aeeb9d3baf8333c39f254
mzubair00 <34907956+mzubair00@users.noreply.github.com>
2022-05-04 15:16:35 -0400

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,65 +1,72 @@
-CREATE TABLE Users(
-    user_id INT NOT NULL,
-    username VARCHAR(16) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    firstname VARCHAR(32) NOT NULL,
-    lastname VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL,
-    create_time TIMESTAMP,
-    permission INT,
-    PRIMARY KEY (username)
-);
+CREATE TABLE `users` (
+  `user_id` int NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `permission` int DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Events(
-    event_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    descript TEXT,
-    create_time TIMESTAMP,
-    time_begin DATETIME,
-    time_end DATETIME,
-    PRIMARY KEY (event_id)
-);
+CREATE TABLE `proevents` (
+  `event_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `descript` text,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `time_begin` datetime DEFAULT NULL,
+  `time_end` datetime DEFAULT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Attendants(
-    list_id INT NOT NULL,
-    event_id INT NOT NULL,
-    user_id INT NOT NULL,
-    confirmation VARCHAR(45),
-    comments TEXT,
-    PRIMARY KEY (list_id),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
+CREATE TABLE `attendants` (
+  `list_id` int NOT NULL,
+  `event_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `confirmation` varchar(45) DEFAULT NULL,
+  `comments` text,
+  PRIMARY KEY (`list_id`),
+  KEY `event_id_idx` (`event_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `event_id` FOREIGN KEY (`event_id`) REFERENCES `proevents` (`event_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Topics(
-    topic_id INT NOT NULL,
-    subj VARCHAR(255),
-    body TEXT,
-    create_time TIMESTAMP,
-    PRIMARY KEY (topic_id)
-);
 
-CREATE TABLE Posts(
-    post_id INT NOT NULL,
-    topic_id INT NOT NULL,
-    user_id INT NOT NULL,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    create_time DATETIME,
-    PRIMARY KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (topic_id) REFERENCES Topics(topic_id)
-);
+CREATE TABLE `topics` (
+  `topic_id` int NOT NULL,
+  `subj` varchar(255) DEFAULT NULL,
+  `body` text,
+  `create_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`topic_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Replies(
-    reply_id INT NOT NULL,
-    post_id INT NOT NULL,
-    topic_id INT NOT NULL,
-    user_id INT NOT NULL,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    create_time DATETIME,
-    PRIMARY KEY (reply_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (post_id) REFERENCES Posts(post_id)
-);
+CREATE TABLE `posts` (
+  `post_id` int NOT NULL,
+  `topic_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `title` text NOT NULL,
+  `body` text NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `topic_id_idx` (`topic_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `topic_id` FOREIGN KEY (`topic_id`) REFERENCES `topics` (`topic_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `replies` (
+  `reply_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  `topic_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `title` text NOT NULL,
+  `body` text NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`reply_id`),
+  KEY `post_id_idx` (`post_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
