c42ea2a3adb6a4cfd6d0a8d47d83b7fb3a96f064
mzubair00 <34907956+mzubair00@users.noreply.github.com>
2022-05-04 15:38:23 -0400

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,27 +1,27 @@
 CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `username` varchar(16) NOT NULL,
   `email` varchar(255) NOT NULL,
   `firstname` varchar(32) NOT NULL,
   `lastname` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `create_time` timestamp NULL DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `permission` int DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `proevents` (
-  `event_id` int NOT NULL AUTO_INCREMENT,
+  `event_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
   `descript` text,
-  `create_time` timestamp NULL DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `time_begin` datetime DEFAULT NULL,
   `time_end` datetime DEFAULT NULL,
   PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `attendants` (
-  `list_id` int NOT NULL AUTO_INCREMENT,
+  `list_id` int NOT NULL,
   `event_id` int NOT NULL,
   `user_id` int NOT NULL,
   `confirmation` varchar(45) DEFAULT NULL,
@@ -35,20 +35,20 @@ CREATE TABLE `attendants` (
 
 
 CREATE TABLE `topics` (
-  `topic_id` int NOT NULL AUTO_INCREMENT,
+  `topic_id` int NOT NULL,
   `subj` varchar(255) DEFAULT NULL,
   `body` text,
-  `create_time` timestamp NULL DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`topic_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `posts` (
-  `post_id` int NOT NULL AUTO_INCREMENT,
+  `post_id` int NOT NULL,
   `topic_id` int NOT NULL,
   `user_id` int NOT NULL,
   `title` text NOT NULL,
   `body` text NOT NULL,
-  `create_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`post_id`),
   KEY `topic_id_idx` (`topic_id`),
   KEY `user_id_idx` (`user_id`),
@@ -57,18 +57,16 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `replies` (
-  `reply_id` int NOT NULL AUTO_INCREMENT,
+  `reply_id` int NOT NULL,
   `post_id` int NOT NULL,
   `topic_id` int NOT NULL,
   `user_id` int NOT NULL,
   `title` text NOT NULL,
   `body` text NOT NULL,
-  `create_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`reply_id`),
   KEY `post_id_idx` (`post_id`),
   KEY `user_id_idx` (`user_id`),
-  KEY `topic_id_idx` (`topic_id`),
-  CONSTRAINT `topics_ibfk` FOREIGN KEY (`topic_id`) REFERENCES `topics` (`topic_id`),
   CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
