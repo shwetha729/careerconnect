5068ab3b4f48183af39a163ee63c25f09d6b7c12
mzubair00 <34907956+mzubair00@users.noreply.github.com>
2022-05-08 14:15:53 -0400

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,48 +1,26 @@
-CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(16) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `firstname` varchar(32) NOT NULL,
-  `lastname` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `location` varchar(32) NOT NULL,
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `permission` int DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+CREATE TABLE `attendees` (
+  `event_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `comments` text,
+  PRIMARY KEY (`event_id`,`user_id`),
+  KEY `event_id_idx` (`event_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `event_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`event_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `proevents` (
+CREATE TABLE `events` (
   `event_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
-  `descript` text,
+  `description` text,
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `time_begin` datetime DEFAULT NULL,
   `time_end` datetime DEFAULT NULL,
-  PRIMARY KEY (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `attendants` (
-  `list_id` int NOT NULL AUTO_INCREMENT,
-  `event_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `confirmation` varchar(45) DEFAULT NULL,
-  `comments` text,
-  PRIMARY KEY (`list_id`),
-  KEY `event_id_idx` (`event_id`),
+  `location` text,
+  PRIMARY KEY (`event_id`),
   KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `event_id` FOREIGN KEY (`event_id`) REFERENCES `proevents` (`event_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `topics` (
-  `topic_id` int NOT NULL AUTO_INCREMENT,
-  `subj` varchar(255) DEFAULT NULL,
-  `body` text,
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`topic_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+  CONSTRAINT `user_id2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `posts` (
   `post_id` int NOT NULL AUTO_INCREMENT,
   `topic_id` int NOT NULL,
@@ -55,12 +33,10 @@ CREATE TABLE `posts` (
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   CONSTRAINT `topic_id` FOREIGN KEY (`topic_id`) REFERENCES `topics` (`topic_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `replies` (
   `reply_id` int NOT NULL AUTO_INCREMENT,
   `post_id` int NOT NULL,
-  `topic_id` int NOT NULL,
   `user_id` int NOT NULL,
   `title` text NOT NULL,
   `body` text NOT NULL,
@@ -70,4 +46,23 @@ CREATE TABLE `replies` (
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `topics` (
+  `topic_id` int NOT NULL AUTO_INCREMENT,
+  `subj` varchar(255) DEFAULT NULL,
+  `body` text,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`topic_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `location` varchar(32) NOT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `permission` int DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
